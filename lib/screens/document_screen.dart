449693b9fc2be3369a1c3f41ffdffb959a3c810b
MaharajMM <maharajmadanmohanofficial@gmail.com>
2022-12-05 02:20:23 +0530

    Autosave & share button completed

--- lib/screens/document_screen.dart
+++ lib/screens/document_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:docs_clone_flutter/constant/import.dart';
+import 'package:docs_clone_flutter/repository/socket_repository.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_quill/flutter_quill.dart' as quill;
 
 class DocumentScreen extends ConsumerStatefulWidget {
@@ -12,13 +16,30 @@ class DocumentScreen extends ConsumerStatefulWidget {
 class _DocumentScreenState extends ConsumerState<DocumentScreen> {
   TextEditingController titleController =
       TextEditingController(text: 'Untitled Document');
-  quill.QuillController _controller = quill.QuillController.basic();
+  quill.QuillController? _controller;
   ErrorModel? errorModel;
+  SocketRepository socketRepository = SocketRepository();
 
   @override
   void initState() {
     super.initState();
+    socketRepository.joinRoom(widget.id);
     fetchDocumentData();
+
+    socketRepository.changeListener((data) {
+      _controller?.compose(
+        quill.Delta.fromJson(data['delta']),
+        _controller?.selection ?? const TextSelection.collapsed(offset: 0),
+        quill.ChangeSource.REMOTE,
+      );
+    });
+
+    Timer.periodic(const Duration(seconds: 3), (timer) {
+      socketRepository.autoSave(<String, dynamic>{
+        'delta': _controller!.document.toDelta(),
+        'room': widget.id,
+      });
+    });
   }
 
   void fetchDocumentData() async {
@@ -28,8 +49,25 @@ class _DocumentScreenState extends ConsumerState<DocumentScreen> {
         );
     if (errorModel!.data != null) {
       titleController.text = (errorModel!.data as DocumentModel).title;
+      _controller = quill.QuillController(
+        document: errorModel!.data.content.isEmpty
+            ? quill.Document()
+            : quill.Document.fromDelta(
+                quill.Delta.fromJson(errorModel!.data.content),
+              ),
+        selection: const TextSelection.collapsed(offset: 0),
+      );
       setState(() {});
     }
+    _controller!.document.changes.listen((event) {
+      if (event.item3 == quill.ChangeSource.LOCAL) {
+        Map<String, dynamic> map = {
+          'delta': event.item2,
+          'room': widget.id,
+        };
+        socketRepository.typing(map);
+      }
+    });
   }
 
   @override
@@ -48,6 +86,11 @@ class _DocumentScreenState extends ConsumerState<DocumentScreen> {
 
   @override
   Widget build(BuildContext context) {
+    if (_controller == null) {
+      return const Scaffold(
+        body: Loader(),
+      );
+    }
     return Scaffold(
         appBar: AppBar(
           backgroundColor: kWhite,
@@ -57,7 +100,23 @@ class _DocumentScreenState extends ConsumerState<DocumentScreen> {
               padding: const EdgeInsets.all(10.0),
               child: ElevatedButton.icon(
                 label: const Text('Share'),
-                onPressed: () {},
+                onPressed: () {
+                  Clipboard.setData(
+                    ClipboardData(
+                      text: 'http://localhost:3000/#/document/${widget.id}',
+                    ),
+                  ).then(
+                    (value) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text(
+                            'Link Copied!',
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
                 icon: const Icon(
                   Icons.lock,
                   size: 16,
@@ -70,15 +129,20 @@ class _DocumentScreenState extends ConsumerState<DocumentScreen> {
             padding: const EdgeInsets.symmetric(vertical: 10.0),
             child: Row(
               children: [
-                Image.asset(
-                  'assets/images/docs-logo.png',
-                  height: 35,
+                GestureDetector(
+                  onTap: (() {
+                    Routemaster.of(context).replace('/');
+                  }),
+                  child: Image.asset(
+                    'assets/images/docs-logo.png',
+                    height: 35,
+                  ),
                 ),
                 const SizedBox(
                   width: 15,
                 ),
                 SizedBox(
-                  width: 200,
+                  width: 150,
                   child: TextField(
                     controller: titleController,
                     decoration: const InputDecoration(
@@ -114,7 +178,7 @@ class _DocumentScreenState extends ConsumerState<DocumentScreen> {
               const SizedBox(
                 height: 10,
               ),
-              quill.QuillToolbar.basic(controller: _controller),
+              quill.QuillToolbar.basic(controller: _controller!),
               const SizedBox(
                 height: 10,
               ),
@@ -127,7 +191,7 @@ class _DocumentScreenState extends ConsumerState<DocumentScreen> {
                     child: Padding(
                       padding: const EdgeInsets.all(30.0),
                       child: quill.QuillEditor.basic(
-                        controller: _controller,
+                        controller: _controller!,
                         readOnly: false,
                       ),
                     ),
--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const http = require("http");
 const authRouter = require("./routes/auth");
 const documentRouter = require("./routes/document");
 
 const PORT = process.env.PORT | 3001;
 const app = express();
+var server = http.createServer(app);
+var io = require('socket.io')(server);
 
 app.use(cors());
 app.use(express.json());
@@ -23,7 +26,26 @@ mongoose.connect(DB).then(() => {
     console.log(err);
 });
 
-app.listen(PORT, "0.0.0.0", () => {
-    console.log("Connected at port 3001");
-    console.log("Hey it is changing");
-})
+io.on("connection", (socket) => {
+    socket.on('join', (documentId)=>{
+        socket.join(documentId);
+        console.log("joined!");
+    });
+    socket.on("typing", (data) => {
+        socket.broadcast.to(data.room).emit("changes", data);
+    });
+    socket.on("save", (data) => {
+        saveData(data);
+        //io.to();
+    });
+});
+
+const saveData = async (data) => {
+    let document = await Document.findById(data.room);
+    document.content = data.delta;
+    document = await document.save();
+};
+
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Connected at port ${PORT}`);
+});
